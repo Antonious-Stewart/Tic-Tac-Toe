4c0fe1c312d3d6490c3bc1fe6ba1a1a59ee353d8
Antonious-Stewart <antonious.stewart@gmail.com>
2018-08-06 20:44:25 -0400

    first message

---
+++ assest/Js/app.js
@@ -0,0 +1,10 @@
+window.onload = ()=>{
+  const playerX = prompt("Your X's! whats your name?: ");
+  const playerO = prompt("Your O's! whats your name?: ");
+  if(typeof playerO ===  "string" && typeof playerX ===  "string"){
+    const X = document.querySelector('.X');
+    const O = document.querySelector('.O');
+    X.textContent = playerX;
+    O.textContent = playerO;
+  }
+}
